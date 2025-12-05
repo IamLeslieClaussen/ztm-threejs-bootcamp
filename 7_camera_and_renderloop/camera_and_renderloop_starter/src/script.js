dee61d7f2f0c294e11509be074b9b3538877f3ac
Leslie Claussen <leslie.claussen@mail.com>
2025-12-05 05:28:26 +0100

    latest commit

--- 7_camera_and_renderloop/camera_and_renderloop_starter/src/script.js
+++ 7_camera_and_renderloop/camera_and_renderloop_starter/src/script.js
@@ -1,39 +1,43 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-console.log(OrbitControls)
+console.log(OrbitControls);
 
 // initialize the scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 // add objects to the scene
-const cubeGeometry = new THREE.BoxGeometry(1,1,1)
-const cubeMaterial = new THREE.MeshBasicMaterial({color: "red"})
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red" });
 
-const cubeMesh = new THREE.Mesh(
-  cubeGeometry,
-  cubeMaterial
-)
-scene.add(cubeMesh)
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cubeMesh);
 
 // initialize the camera
 const camera = new THREE.PerspectiveCamera(
-  75, 
+  75,
   window.innerWidth / window.innerHeight,
   0.1,
-  200)
-camera.position.z = 5
+  200
+);
+camera.position.z = 5;
 
 // initialize the renderer
-const canvas = document.querySelector('canvas.threejs')
+const canvas = document.querySelector("canvas.threejs");
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
-})
-
+  canvas: canvas,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
 
 //instantiate the controls
-const controls = new OrbitControls(camera, canvas)
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.autoRotate = true;
 
+const renderloop = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(renderloop);
+};
 
-renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.render(scene, camera)
+renderloop();
